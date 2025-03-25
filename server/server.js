9abb1f0426f9d54ed913ee5dc742ccc7e1505346
`Oguzhan <oguzhan_duran@epam.com>
2025-03-25 09:18:08 +0300

    fetched data from created nodejs server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require("cors")
+const PORT = 8080;
+
+app.use(cors());
+
+
+// creating api route
+app.get("/api/home", (req, res) => {
+    res.json({ message: "Hello World!", people: ["oguzhan", "hasan", "kaan"] });
+})
+
+// To run application
+app.listen(PORT, () => {
+console.log(`Server started on port ${PORT}`)
+})
